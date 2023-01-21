595b81a997f25fc9785a3f2c78689727134bb3f8
evariste1963 <evariste@hotmail.co.uk>
2023-01-21 10:13:58 +0000

    1st commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Forms</h1>
+
+
+<form method="POST"  action="/login">
+<input type="text" name="user" />
+<input type="password" name="password" />
+<button type="submit">Login</button>
+</form>
